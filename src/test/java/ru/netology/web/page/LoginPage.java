ed36f70096a2fa348762bc66486121e45b23ae15
Petrov Maxim <maxim.petrov@1ci.com>
2023-07-28 01:50:17 +0300

    initial commit

---
+++ src/test/java/ru/netology/web/page/LoginPage.java
@@ -0,0 +1,25 @@
+package ru.netology.web.page;
+
+import com.codeborne.selenide.SelenideElement;
+import org.openqa.selenium.support.FindBy;
+import ru.netology.web.data.DataHelper;
+
+import static com.codeborne.selenide.Selenide.page;
+
+public class LoginPage {
+    @FindBy(css = "[data-test-id=login] input")
+    private static SelenideElement loginField;
+
+    @FindBy(css = "[data-test-id=password] input")
+    private static SelenideElement passwordField;
+    @FindBy(css = "[data-test-id=action-login]")
+    private static SelenideElement loginButton;
+
+    public VerificationPage validLogin(DataHelper.AuthInfo info) {
+        loginField.setValue(info.getLogin());
+        passwordField.setValue(info.getPassword());
+        loginButton.click();
+        return page(VerificationPage.class);
+    }
+
+}
